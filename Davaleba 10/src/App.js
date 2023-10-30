0ec1e4ea378eaf0e7c9ab9c90f5cb61d709640a8
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-10-30 22:25:09 +0400

    adding davaleba 10

---
+++ Davaleba 10/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import img from './assets/1.jpg'
+
+function App() {
+  return (
+    <div className="App">
+<h1>Image </h1>  
+
+<img src={img} alt=''/>
+    </div>
+  );
+}
+
+export default App;
