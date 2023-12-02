64db190a47e3f9004105d8aa0a5fc2f937634ddd
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-12-02 22:18:05 +0400

    adding davaleba 17

---
+++ Davaleba 17/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './styles/styles.css'; 
+import useLocalStorage from './hooks/useLocalStorage';
+import useDetectDevice from './hooks/useDetectDevice';
+
+function App() {
+  const [themeColor, toggle] = useLocalStorage('theme', true)
+  const device = useDetectDevice()
+ console.log(device);
+
+  return (
+    <div>
+    {device === 'MOBILE' ? (
+      <>
+        <h1>Hello, Mobile User!</h1>
+      </>
+    ) : (
+      <><div className={`fullscreen-${themeColor ? 'light' : 'dark'}`}>
+        <h1>Hello, Desktop User!</h1>
+        <button onClick={() => toggle(prev => !prev)}>Toggle Theme</button>
+        </div>   
+      </>
+    )}
+    </div>
+  );
+}
+
+export default App;
