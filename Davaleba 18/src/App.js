4186d1bfeee1fa8146f65c395baca2e053f02cd0
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-12-02 23:28:53 +0400

    adding davaleba 18

---
+++ Davaleba 18/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Fact from './components/Fact';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/:factId" element={<Fact />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
